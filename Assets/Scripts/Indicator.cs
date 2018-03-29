08be9046f44f3d3cb1daf31a404bcd39018ebec5
bli-desktop <bli@trinity.edu>
2018-03-28 21:11:49 -0500

    Initial commit

---
+++ Assets/Scripts/Indicator.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Indicator : MonoBehaviour {
+
+    public GameObject target;
+    public GameObject indicator;
+    public float indicatorOffset;
+    public float detectingOffset;
+    private Camera cam;
+    private Vector3 targetPosOnScreen;
+    private Vector3 displayPos;
+    private Vector3 center;
+ 
+
+    void Start () {
+        cam = Camera.main;
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        targetPosOnScreen = cam.WorldToViewportPoint(target.transform.position);
+
+        // Debug.Log("indicatorPos " + indicator.transform.position);
+
+        if (targetPosOnScreen.x >= 0f - detectingOffset &&
+            targetPosOnScreen.x <= 1f + detectingOffset &&
+            targetPosOnScreen.y >= 0f - detectingOffset &&
+            targetPosOnScreen.y <= 1f + detectingOffset) {
+            indicator.SetActive(false);
+        } else {
+            if (!indicator.activeSelf) {
+                indicator.SetActive(true);
+            }
+            center = cam.WorldToViewportPoint( transform.position);
+            Debug.Log("target " + targetPosOnScreen);
+            
+            float angle = Mathf.Atan2(targetPosOnScreen.y - center.y, targetPosOnScreen.x - center.x) * Mathf.Rad2Deg;
+            indicator.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angle)); //Quaternion.AngleAxis(angle, Vector3.forward);
+
+            displayPos = new Vector3( Mathf.Clamp( targetPosOnScreen.x, indicatorOffset, 1f-indicatorOffset), Mathf.Clamp(targetPosOnScreen.y, indicatorOffset, 1f- indicatorOffset), 0f);
+            indicator.transform.position = cam.ViewportToScreenPoint(displayPos);
+            Debug.Log("display " + displayPos);
+
+
+        }
+    }
+}
---
+++ Assets/Scripts/MapManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+
+
+    public static MapManager Instance;
+
+    public GameObject unitRock;
+    public GameObject unitWall;
+
+    private float rockScale;
+    private float yPosition;
+    private Dictionary<string, GameObject> rocks;
+
+
+    void Awake() {
+        Instance = this;
+        rocks = new Dictionary<string, GameObject>();
+        rockScale = unitRock.transform.localScale.x;
+        yPosition = unitRock.transform.position.y;
+        InitializeMap(100);
+    }
+
+    private void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void InitializeMap(int dim) {
+        for (int x = 0; x < dim; x++) {
+            for (int z = 0; z < dim; z++) {
+                if (x == 0 || x == dim - 1 || z == 0 || z == dim - 1) {
+                    //Instantiate(
+                    //    unitWall,
+                    //    new Vector3((float)x * rockScale, yPosition, (float)z * rockScale),
+                    //    Quaternion.identity);
+                } else {
+                    GameObject rock = Instantiate(
+                        unitRock,
+                        new Vector3((float)x * rockScale, yPosition, (float)z * rockScale),
+                        Quaternion.identity);
+                    rock.name = "UnitRock" + " (" + x + "," + z + ")";
+                    rocks.Add(rock.name, rock);
+                }
+            }
+        }
+    }
+
+
+    public void DestroyRocks(List<string> names) {
+        Debug.Log("length " + names.Count);
+        foreach (string name in names) {
+            if (rocks[name] != null) {
+                rocks[name].SetActive(false);
+            }
+        }
+    }
+
+
+}
+
